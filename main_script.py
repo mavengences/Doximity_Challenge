cb49a745d6564b7daf1eaa806ff2f54c52c135cb
mavengences <dorienxia74@gmail.com>
2017-11-18 16:38:52 -0600

    updating output to github

--- main_script.py
+++ main_script.py
@@ -20,6 +20,10 @@ from pandas.io import sql
 import sqlite3
 from sqlalchemy import create_engine
 import psycopg2
+from datetime import datetime
+
+
+startTime = datetime.now()
 
 def api_to_dataframe(api_endpoint):
     first_list=requests.get(api_endpoint).json()
@@ -75,25 +79,42 @@ def html_url_table_to_dataframe(url):
 
 
 
-for i in range(1,4,1):
+for i in range(1,152,1):
     url="http://de-tech-challenge-api.herokuapp.com/user_activity?page="+str(i)
     output_df=html_url_table_to_dataframe(url)
-    print(output_df.head(10))
+    print(output_df.head(1))
     print("\n")
     api_endpoint="http://de-tech-challenge-api.herokuapp.com/api/v1/users?page="+str(i)
     output_api_dataframe=api_to_dataframe(api_endpoint)
-    print(output_api_dataframe.head(10))
+    print(output_api_dataframe.head(1))
     print("\n")
     if i ==1:
-        final_output_df=output_df.merge(output_api_dataframe, left_on=['ID','Firstname','Lastname'], right_on=['ID','Firstname','Lastname'], how="left")
-        print(final_output_df.head(10))
+        final_output_df=output_df.merge(output_api_dataframe, left_on=['ID','Firstname','Lastname'], right_on=['ID','Firstname','Lastname'], how="inner")
+        print("five sample rows can be seen as: \n")
+        print(final_output_df.head(5))
+        print("total dataframe count is: \n")
+        print(final_output_df.count())
     else:
-        merged_df=output_df.merge(output_api_dataframe, left_on=['ID','Firstname','Lastname'], right_on=['ID','Firstname','Lastname'], how="left")
+        merged_df=output_df.merge(output_api_dataframe, left_on=['ID','Firstname','Lastname'], right_on=['ID','Firstname','Lastname'], how="inner")
         final_output_df=final_output_df.append(merged_df, ignore_index=True)
+        print("ten sample rows can be seen as: \n")
         print(merged_df.head(10))
         print("merged dataframe count is: \n")
         print(merged_df.count())
         print("total dataframe count is: \n")
         print(final_output_df.count())
 
+final_output_df.to_csv("output.txt", sep="\x01", header=False)
+
+schema_creation_output_df=final_output_df.head(5)
+
+schema_creation_output_df.to_csv("schema.txt", sep="\x01", header=True)
+        
+print(" \n total time it took to run this script was: \n")   
+script_runtime = datetime.now() - startTime  
+print(script_runtime)
+
+with open("metrics.txt", "w") as fout:
+    fout.write("script runtime is: "+str(script_runtime)+"\n")
+    fout.write("total number of rows processed is: "+str(final_output_df.count())+"\n")
 
